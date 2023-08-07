1d34225a96f6dcc54c9da6f6292c66748fbed6a1
Gabriel Sandoval <gabrielsandoval1020@gmail.com>
2023-08-07 20:05:08 +0800

    Add rails db:create and migrate tasks

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -20,11 +20,12 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
 }
 
 resource "aws_ecs_service" "my_app_service" {
-  name            = "my_app_service"     # Name the service
-  cluster         = "${aws_ecs_cluster.my_app_cluster.id}"   # Reference the created Cluster
-  task_definition = "${aws_ecs_task_definition.my_app_task.arn}" # Reference the task that the service will spin up
-  launch_type     = "FARGATE"
-  desired_count   = 3 # Set up the number of containers to 2
+  name                              = "my_app_service"     # Name the service
+  cluster                           = "${aws_ecs_cluster.my_app_cluster.id}"   # Reference the created Cluster
+  task_definition                   = "${aws_ecs_task_definition.my_app_task.arn}" # Reference the task that the service will spin up
+  launch_type                       = "FARGATE"
+  desired_count                     = 3 # Set up the number of containers to 2
+  health_check_grace_period_seconds = 300 # 5 minutes
 
   load_balancer {
     target_group_arn = "${aws_lb_target_group.target_group.arn}" # Reference the target group
@@ -38,69 +39,3 @@ resource "aws_ecs_service" "my_app_service" {
     security_groups  = ["${aws_security_group.service_security_group.id}"] # Set up the security group
   }
 }
-
-resource "aws_ecs_task_definition" "my_app_task" {
-  family                   = "my_app_task" # Name your task
-  container_definitions    = <<DEFINITION
-  [
-    {
-      "name": "my_app_task",
-      "image": "${aws_ecr_repository.my_app_repo.repository_url}",
-      "essential": true,
-      "logConfiguration":{
-        "logDriver": "awslogs",
-        "options": {
-          "awslogs-group": "${aws_cloudwatch_log_group.my_app.name}",
-          "awslogs-region": "us-east-1",
-          "awslogs-stream-prefix": "web"
-        }
-      },
-      "environment": [
-        {
-          "name": "RAILS_ENV",
-          "value": "production"
-        },
-        {
-          "name": "HOST",
-          "value": "${aws_alb.my_app_load_balancer.dns_name}"
-        },
-        {
-          "name": "DATABASE_HOST",
-          "value": "${aws_db_instance.rds.address}"
-        },
-        {
-          "name": "DATABASE_NAME",
-          "value": "${aws_db_instance.rds.db_name}"
-        },
-        {
-          "name": "DATABASE_USERNAME",
-          "value": "${aws_db_instance.rds.username}"
-        },
-        {
-          "name": "DATABASE_PASSWORD",
-          "value": "${aws_db_instance.rds.password}"
-        }
-      ],
-      "portMappings": [
-        {
-          "containerPort": 3000,
-          "hostPort": 3000
-        }
-      ],
-      "memory": 512,
-      "cpu": 256
-    }
-  ]
-  DEFINITION
-  requires_compatibilities = ["FARGATE"] # use Fargate as the launch type
-  network_mode             = "awsvpc"    # add the AWS VPN network mode as this is required for Fargate
-  memory                   = 512         # Specify the memory the container requires
-  cpu                      = 256         # Specify the CPU the container requires
-  execution_role_arn       = "${aws_iam_role.ecs_task_execution_role.arn}"
-}
-
-# I was initially gettting this:
-# ActiveRecord::DatabaseConnectionError (There is an issue connecting with your hostname: my-app-db.cbbxmhkhubl8.us-east-1.rds.amazonaws.com:3306.
-#
-# Then after removing the port from the database endpoint, I now get this:
-# ActiveRecord::ConnectionNotEstablished (Can't connect to server on 'my-app-db.cbbxmhkhubl8.us-east-1.rds.amazonaws.com' (110)):
